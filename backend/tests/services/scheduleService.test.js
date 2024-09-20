c7303cce7cb633f9a1bb63232946224e0078ad8c
Julian Willis <jmwillis.2022@business.smu.edu.sg>
2024-09-20 10:35:13 +0800

    test(view team schedule): Add unit test for GET team schedule [IS-3]
    - add unit test for get team schedule service

--- backend/tests/services/scheduleService.test.js
+++ backend/tests/services/scheduleService.test.js
@@ -4,7 +4,8 @@ const { Op } = require('sequelize');
 // Mock the `Staff` and `Schedule` models
 jest.mock('../../models', () => ({
     Staff: {
-        findAll: jest.fn()
+        findAll: jest.fn(),
+        findOne: jest.fn()
     },
     Schedule: {
         findAll: jest.fn(),
@@ -313,5 +314,255 @@ describe('Schedule Service', () => {
 
             expect(result).toEqual(expectedOutput);
         });
+    });
+
+    describe('getScheduleByTeam', () => {
+        test('should retrieve schedules by team', async () => {
+            const staff_id = 1;
+            const departmentname = 'Engineering';
+            const position = 'Developer';
+            const start_date = '2023-10-01';
+            const end_date = '2023-10-07';
+
+            // Mock data for `Staff.findOne`
+            const staffData1 = { staff_id: 1, staff_fname: 'John', staff_lname: 'Doe', dept: departmentname, position };
+
+            // Mock data for `Staff.findAll`
+            const staffData = [
+                { staff_id: 1, staff_fname: 'John', staff_lname: 'Doe', dept: departmentname, position },
+                { staff_id: 2, staff_fname: 'Jane', staff_lname: 'Smith', dept: departmentname, position }
+            ];
+            
+            const scheduleData = [
+                { staff_id: 1, start_date: '2023-10-01', session_type: 'Work from home', Staff: staffData[0]},
+                { staff_id: 2, start_date: '2023-10-02', session_type: 'Work from home', Staff: staffData[1]}
+            ];
+            
+            // Set up mocks
+            Schedule.findAll.mockResolvedValue(scheduleData);
+            Staff.findOne.mockResolvedValue(staffData1);
+            Staff.findAll.mockResolvedValue(staffData);
+
+
+            const result = await scheduleService.getScheduleByTeam({ staff_id, start_date, end_date });
+            console.log(result);
+            expect(Staff.findAll).toHaveBeenCalledWith({
+                where: {
+                    dept: departmentname,
+                    ...(position && { position })
+                }
+            });
+
+            expect(Schedule.findAll).toHaveBeenCalledWith({
+                where: {
+                    start_date: { [Op.gte]: start_date, [Op.lte]: end_date }
+                },
+                include: [
+                    {
+                        model: Staff,
+                        where: {
+                            dept: departmentname,
+                            ...(position && { position }),
+                        },
+                    }
+                ]
+            });
+
+            const expectedOutput = {
+                '2023-10-01': {
+                    'Engineering': {
+                        'Developer': {
+                            'In office': ['Jane Smith'],
+                            'Work from home': ['John Doe'],
+                            'Work from home (AM)': [],
+                            'Work from home (PM)': [],
+                        }
+                    }
+                },
+                '2023-10-02': {
+                    'Engineering': {
+                        'Developer': {
+                            'In office': ['John Doe'],
+                            'Work from home': ['Jane Smith'],
+                            'Work from home (AM)': [],
+                            'Work from home (PM)': [],
+                        }
+                    }
+                },
+                '2023-10-03': {
+                    'Engineering': {
+                        'Developer': {
+                            'In office': ['John Doe', 'Jane Smith'],
+                            'Work from home': [],
+                            'Work from home (AM)': [],
+                            'Work from home (PM)': [],
+                        }
+                    }
+                },
+                '2023-10-04': {
+                    'Engineering': {
+                        'Developer': {
+                            'In office': ['John Doe', 'Jane Smith'],
+                            'Work from home': [],
+                            'Work from home (AM)': [],
+                            'Work from home (PM)': [],
+                        }
+                    }
+                },
+                '2023-10-05': {
+                    'Engineering': {
+                        'Developer': {
+                            'In office': ['John Doe', 'Jane Smith'],
+                            'Work from home': [],
+                            'Work from home (AM)': [],
+                            'Work from home (PM)': [],
+                        }
+                    }
+                },
+                '2023-10-06': {
+                    'Engineering': {
+                        'Developer': {
+                            'In office': ['John Doe', 'Jane Smith'],
+                            'Work from home': [],
+                            'Work from home (AM)': [],
+                            'Work from home (PM)': [],
+                        }
+                    }
+                },
+                '2023-10-07': {
+                    'Engineering': {
+                        'Developer': {
+                            'In office': ['John Doe', 'Jane Smith'],
+                            'Work from home': [],
+                            'Work from home (AM)': [],
+                            'Work from home (PM)': [],
+                        }
+                    }
+                }
+            };
+
+            expect(result).toEqual(expectedOutput);
+        });
+
+
+        test('should handle no schedules for staff', async () => {
+            const staff_id = 1;
+            const departmentname = 'Engineering';
+            const position = 'Developer';
+            const start_date = '2023-10-01';
+            const end_date = '2023-10-07';
+
+             // Mock data for `Staff.findOne`
+            const staffData1 = { staff_id: 1, staff_fname: 'John', staff_lname: 'Doe', dept: departmentname, position };
+
+            const staffData = [
+                { staff_id: 1, staff_fname: 'John', staff_lname: 'Doe', dept: departmentname, position: position },
+                { staff_id: 2, staff_fname: 'Jane', staff_lname: 'Smith', dept: departmentname, position: position }
+            ];
+
+            Staff.findAll.mockResolvedValue(staffData);
+            Staff.findOne.mockResolvedValue(staffData1); 
+            Schedule.findAll.mockResolvedValue([]);
+
+            const result = await scheduleService.getScheduleByTeam({ staff_id, start_date, end_date });
+
+            expect(Staff.findAll).toHaveBeenCalledWith({
+                where: {
+                    dept: departmentname,
+                    ...(position && { position })
+                }
+            });
+
+            expect(Schedule.findAll).toHaveBeenCalledWith({
+                where: {
+                    start_date: { [Op.gte]: start_date, [Op.lte]: end_date }
+                },
+                include: [
+                    {
+                        model: Staff,
+                        where: {
+                            dept: departmentname,
+                            ...(position && { position }),
+                        },
+                    }
+                ]
+            });
+
+            const expectedOutput = {
+                '2023-10-01': {
+                    'Engineering': {
+                        'Developer': {
+                            'In office': ['John Doe', 'Jane Smith'],
+                            'Work from home': [],
+                            'Work from home (AM)': [],
+                            'Work from home (PM)': [],
+                        }
+                    }
+                },
+                '2023-10-02': {
+                    'Engineering': {
+                        'Developer': {
+                            'In office': ['John Doe', 'Jane Smith'],
+                            'Work from home': [],
+                            'Work from home (AM)': [],
+                            'Work from home (PM)': [],
+                        }
+                    }
+                },
+                '2023-10-03': {
+                    'Engineering': {
+                        'Developer': {
+                            'In office': ['John Doe', 'Jane Smith'],
+                            'Work from home': [],
+                            'Work from home (AM)': [],
+                            'Work from home (PM)': [],
+                        }
+                    }
+                },
+                '2023-10-04': {
+                    'Engineering': {
+                        'Developer': {
+                            'In office': ['John Doe', 'Jane Smith'],
+                            'Work from home': [],
+                            'Work from home (AM)': [],
+                            'Work from home (PM)': [],
+                        }
+                    }
+                },
+                '2023-10-05': {
+                    'Engineering': {
+                        'Developer': {
+                            'In office': ['John Doe', 'Jane Smith'],
+                            'Work from home': [],
+                            'Work from home (AM)': [],
+                            'Work from home (PM)': [],
+                        }
+                    }
+                },
+                '2023-10-06': {
+                    'Engineering': {
+                        'Developer': {
+                            'In office': ['John Doe', 'Jane Smith'],
+                            'Work from home': [],
+                            'Work from home (AM)': [],
+                            'Work from home (PM)': [],
+                        }
+                    }
+                },
+                '2023-10-07': {
+                    'Engineering': {
+                        'Developer': {
+                            'In office': ['John Doe', 'Jane Smith'],
+                            'Work from home': [],
+                            'Work from home (AM)': [],
+                            'Work from home (PM)': [],
+                        }
+                    }
+                }
+            };
+
+            expect(result).toEqual(expectedOutput);
+        });
+
     });
 });
