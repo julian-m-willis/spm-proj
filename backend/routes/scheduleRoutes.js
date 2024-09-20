9b3953295ee1ee6c0957b058cd739469fc473d9f
Julian Willis <jmwillis.2022@business.smu.edu.sg>
2024-09-20 10:24:38 +0800

    feat(view team schedule): Add API endpoint to view team schedule [IS-3]
    - add api route /schedule/staff/team/?start_date=&end_date=
    - get user's team based on 'position' field on Staff schema.
    - get user details from token

--- backend/routes/scheduleRoutes.js
+++ backend/routes/scheduleRoutes.js
@@ -7,6 +7,7 @@ const authentorizeRole = require('../middleware/authorizeRoleMiddleware');
 router.post('/', authenticateToken, scheduleController.createSchedule);
 router.get('/manager/:departmentname', authenticateToken,  authentorizeRole([2, 3]), scheduleController.getScheduleByDepartment);
 router.get('/hr', authenticateToken,  authentorizeRole([3]), scheduleController.getScheduleByDepartment);
+router.get('/staff/team', authenticateToken, scheduleController.getScheduleByTeam);
 // Implement other routes similarly
 
 module.exports = router;
