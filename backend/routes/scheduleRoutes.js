b5197b7b3f6d49c09bef8239b775914ff1fdcd2e
Julian Willis <jmwillis.2022@business.smu.edu.sg>
2024-09-22 10:44:14 +0800

    refactor(schedule routes): Adjust authorization role to the new csv file

--- backend/routes/scheduleRoutes.js
+++ backend/routes/scheduleRoutes.js
@@ -5,10 +5,9 @@ const authenticateToken = require('../middleware/authenticateTokenMiddleware');
 const authentorizeRole = require('../middleware/authorizeRoleMiddleware');
 
 router.post('/', authenticateToken, scheduleController.createSchedule);
-router.get('/manager/:departmentname', authenticateToken,  authentorizeRole([2, 3]), scheduleController.getScheduleByDepartment);
-router.get('/hr', authenticateToken,  authentorizeRole([3]), scheduleController.getScheduleByDepartment);
+router.get('/manager/:departmentname', authenticateToken,  authentorizeRole([1, 3]), scheduleController.getScheduleByDepartment);
+router.get('/hr', authenticateToken,  authentorizeRole([1]), scheduleController.getScheduleByDepartment);
 router.get('/staff/team', authenticateToken, scheduleController.getScheduleByTeam);
 router.get('/staff', authenticateToken, scheduleController.getSchedulePersonal);
-// Implement other routes similarly
 
 module.exports = router;
