c5719d38ce5e69bd53f5e7cb1d7e00ac1e893a38
Julian Willis <jmwillis.2022@business.smu.edu.sg>
2024-09-25 08:27:19 +0800

    docs: Style formating of package.json

--- frontend/app/(dashboard)/schedule/view-own-schedule/page.jsx
+++ frontend/app/(dashboard)/schedule/view-own-schedule/page.jsx
@@ -1,6 +1,19 @@
-import * as React from 'react';
-import Typography from '@mui/material/Typography';
+"use client";
 
-export default function ViewOwnSchedulePage() {
-  return <Typography>Welcome to the orders page!</Typography>;
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import React, { useEffect, useState } from 'react';
+import { Box, Typography, Paper, Grid, CircularProgress, TextField, MenuItem } from '@mui/material';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import axios from 'axios';
+
+export default function BasicDateCalendar() {
+  return (
+    <Box sx={{ padding: '20px' }}>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DateCalendar />
+      </LocalizationProvider>
+    </Box>
+  );
 }
