bbd5beb59abc7b867d24c5c49690379f0fccae4b
Julian Willis <jmwillis.2022@business.smu.edu.sg>
2024-10-18 15:10:40 +0800

    chore: Update CI Pipeline

--- backend/tests/app.test.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest');
-const app = require('../app');
-
-describe('Test the root path', () => {
-    test('It should respond to the GET method', async () => {
-        const response = await request(app).get('/');
-        expect(response.statusCode).toBe(404); // Assuming there's no route defined for '/'
-    });
-});
