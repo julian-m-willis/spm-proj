f0f2ae28d0934d30e3e70945b020212b32662a5f
Julian Willis <jmwillis.2022@business.smu.edu.sg>
2024-10-27 14:29:39 +0800

    fix: fix API key env [IS-13]

--- backend/services/mailService.js
+++ backend/services/mailService.js
@@ -3,7 +3,7 @@ const Mailgun = require("mailgun.js");
 const mailgun = new Mailgun(formData);
 const mg = mailgun.client({
   username: "api",
-  key: process.env.MAILGUN_API_KEY,
+  key: process.env.MAILGUN_API_KEY || "test-key", // Fallback to "test-key" if not set
 });
 
 exports.sendResetPasswordEmail = async (email, token) => {
